e16104050ccc1b0210d0b3e80202e1936b1b2080
Brad Nissen <bradanissen@gmail.com>
2019-06-28 21:33:03 -0700

    adds some console logging with a star wars API

---
+++ script.js
@@ -0,0 +1,38 @@
+function callAPI(endpointURI) {
+
+    // Create a variable to return
+    var returningString;
+
+    // Create a request variable and sssign a new XMLHttpRequest onject to it
+    var request = new XMLHttpRequest()
+
+    // open a new connection, using the GET request on the URL endpoint
+    request.open('GET', endpointURI, true)
+
+    request.onload = function () {
+
+        //begin accessing the JSON here
+        var data = JSON.parse(this.response)
+
+        if(request.status >= 200 && request.status < 400){
+           
+            for(var i = 0; i < data.results.length; i++){
+                console.log(data.results[i].name)
+            }
+            
+        } else {
+            console.log('error')
+        }
+    }
+
+    //Send request
+    request.send()
+
+}
+
+var endpoint = 'https://swapi.co/api/people/'
+
+callAPI(endpoint);
+
+
+
